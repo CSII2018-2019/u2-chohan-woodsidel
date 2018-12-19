69a82d4990e75fb8cc6c4d393716b52da0ede850
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2018-12-18 22:31:17 -0500

    Finished

--- src/ChoHan.java
+++ src/ChoHan.java
@@ -1,15 +1,98 @@
 import javax.swing.JOptionPane;
 
 public class ChoHan {
-
+	static String rollSide;
+	static String result1;
+	static String result2;
 	public static void main(String[] args) {
-		NormalDice diceOne = new NormalDice(6);
-		NormalDice.setSides(6);
-		NormalDice.getSides();
-		NormalDice.setArraySides();
-		NormalDice.setSubtract(6);
-		NormalDice.getSubtract();
-		JOptionPane.showMessageDialog(null, diceOne.toString());
-	}
-
+		JOptionPane.showMessageDialog(null, "Welcome to ChoHan!");
+		String decision = JOptionPane.showInputDialog("Do you want to play with normal or loaded dice?");
+		if(decision.equals("Loaded")) {
+			Loaded diceOne = new Loaded(6);
+			Loaded.setSides(6);
+			Loaded.getSides();
+			Loaded diceTwo = new Loaded(6);
+			Loaded.setSides(6);
+			Loaded.getSides();
+			JOptionPane.showMessageDialog(null, "Player 1: "+diceOne.toString()+"\nPlayer 2: "+diceTwo.toString());
+			JOptionPane.showMessageDialog(null, "Let's start!");
+			Loaded.randomRoll();
+			int roll = Loaded.getRandomRoll();
+			String bet1 = JOptionPane.showInputDialog(null, "Player 1: What is your bet? Even or Odd");
+			String bet2 = JOptionPane.showInputDialog(null, "Player 2: What is your bet? Even or Odd");
+			if(roll % 2 == 0) {
+				rollSide = "Even";
+			}
+			else {
+				rollSide = "Odd";
+			}
+			if(bet1.equals(rollSide)) {
+				result1 = "WIN";
+			}
+			else {
+				result1 = "LOSE";
+			}
+			if(bet2.equals(rollSide)) {
+				result2 = "WIN";
+			}
+			else {
+				result2 = "LOSE";
+			}
+			if(result1.equals(result2)) {
+				JOptionPane.showMessageDialog(null, "It's a tie!");
+			}
+			
+		}
+		else {
+			NormalDice diceOne = new NormalDice(6);
+			NormalDice.setSides(6);
+			NormalDice.getSides();
+			NormalDice diceTwo = new NormalDice(6);
+			NormalDice.setSides(6);
+			NormalDice.getSides();
+			JOptionPane.showMessageDialog(null, "Player 1: "+diceOne.toString()+"\nPlayer 2: "+diceTwo.toString());
+			JOptionPane.showMessageDialog(null, "Let's start!");
+			NormalDice.randomRoll();
+			int roll1 = NormalDice.getRandomRoll();
+			NormalDice.randomRoll();
+			int roll2 = NormalDice.getRandomRoll();
+			String bet1 = JOptionPane.showInputDialog(null, "Player 1: What is your bet? Even or Odd");
+			String bet2 = JOptionPane.showInputDialog(null, "Player 2: What is your bet? Even or Odd");
+			if((roll1 + roll2) % 2 == 0) {
+				rollSide = "Even";
+			}
+			else {
+				rollSide = "Odd";
+			}
+			if(bet1.equals(rollSide)) {
+				result1 = "WIN";
+			}
+			else {
+				result1 = "LOSE";
+			}
+			if(bet2.equals(rollSide)) {
+				result2 = "WIN";
+			}
+			else {
+				result2 = "LOSE";
+			}
+			JOptionPane.showMessageDialog(null, "The dice were "+rollSide+"!" );
+			if(result1.equals(result2)) {
+				JOptionPane.showMessageDialog(null, "It's A Tie!");
+			}
+			else if(result1.equals("WIN")) {
+				JOptionPane.showMessageDialog(null,"Player 1 Wins!\nPlayer 2 Loses!");
+			}
+			else if(result2.equals("WIN")) {
+				JOptionPane.showMessageDialog(null,"Player 2 Wins!\nPlayer 1 Loses!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Nobody Wins!");
+			}
+			
+		}
+	
+		
+		
+}
 }
--- src/Loaded.java
+++ src/Loaded.java
@@ -1,17 +1,14 @@
 import java.util.Random;
-
-import javax.swing.JOptionPane;
-public class NormalDice{
-	public static int numSides;
+public class Loaded{
+	private static int numSides;
 	private static int min = 1;
-	public static int roll;
+	private static int roll;
 	
-	public NormalDice(int num){
-		setSides(num);
+	public Loaded(int m) {
+		setSides(m);
 		getSides();
 		randomRoll();
 		getRandomRoll();
-	         
 	}
 	public static void setSides(int s) {
 		numSides = s;
@@ -21,17 +18,28 @@ public class NormalDice{
 	}
 	public static void randomRoll() {
 		Random rand = new Random();
-		int roll = rand.nextInt(((numSides-min)+1)+min);
-		
+		roll = rand.nextInt(((numSides-min)+1)+min);
+		if(roll%2 == 0) {
+			roll += 1;
+			if(roll > numSides) {
+				roll -= 2;
+			}
+			else if(roll < min) {
+				roll = 1;
+			}
+		}
 	}
 	public static int getRandomRoll() {
 		return roll;
 	}
+
 	public String toString() {
 		return "Your die has "+numSides+" sides.";
 	}
 	public String rollString() {
 		return "Your roll is: "+roll+" ";
 	}
-	}
+	
+
+}
 
